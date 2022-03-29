7f44efa8c7e3c8f1e45daf68e828fedad4aa267f
andrea-maiolo <maiolo.m.2@gmail.com>
2022-03-29 17:12:50 +0200

    added personal info section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import PersonalInfo from './components/PersonalInfo'
 
 function App() {
+
+  const [personal, setPersonal] = React.useState({})
+
+  const handleSubmit= function(e){
+    e.preventDefault();
+    console.log(personal)
+  }
+
+  const handleInputChange= function(e){
+    let name = e.target.name;
+    let value = e.target.value;
+    setPersonal(prevState => {
+      return{
+        ...prevState,
+        [name]:value
+      }
+    })
+  }
+
   return (
     <div className="App">
-      hello
+      <form>
+        <PersonalInfo onChange={handleInputChange} personal={personal}/>
+        <button type="submit" onClick={handleSubmit}>Submit</button>
+      </form>
     </div>
   );
 }
---
+++ src/components/PersonalInfo.js
@@ -0,0 +1,22 @@
+import React, {Component, useState, useEffect} from 'react';
+
+class PersonalInfo extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+      return (
+        <div className="personalDiv">
+            <input onChange={this.props.onChange} type="text" className="personalInput" placeholder="First Name" name="firstName" required></input>
+            <input onChange={this.props.onChange} type="text" className="personalInput" placeholder="Last Name" name="lastName" required></input>
+            <input type="file" className="personalInput" name="photo" accept="image/png, image/jpeg"></input>
+            <input onChange={this.props.onChange} type="text" className="personalInput" placeholder="Address" name="address"></input>
+            <input onChange={this.props.onChange} type="email" className="personalInput" placeholder="Email" name="email" required></input>
+            <input onChange={this.props.onChange} type="text" className="personalInput" placeholder="Phone number" name="phoneNumber" required></input>
+            <input onChange={this.props.onChange} type="text" className="personalInput" placeholder="Description" name="description"></input>
+        </div>
+      )
+    }
+}
+
+export default PersonalInfo;
